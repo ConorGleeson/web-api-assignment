77ae413b4e8402f49455133d427142c09b528592
Conor Gleeson <conorgleeson20093384@gmail.com>
2022-12-28 21:39:11 +0000

    cant access homepage actually error in movie list

--- reactApp/src/api/tmdb-api.js
+++ reactApp/src/api/tmdb-api.js
@@ -1,38 +1,38 @@
 
 
 
-// export const getMovies = (args) => {
-//   const[,sortMovieBy] = args.queryKey;
-//   const[, pageNumPart] = args.queryKey; //based off of movies id below
-//   const {pageNum} = pageNumPart; 
-//   const {sort} =sortMovieBy;
-//   console.log(pageNum)
-//   return fetch(
-//     `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${pageNum}`
-//   ).then((response) => {
-//     if (!response.ok) {
-//       throw new Error(response.json().message);
-//     }
-//     return response.json();
-//   })
-//   .catch((error) => {
-//      throw error
-//   });
-// };
-
-export const getMovies = () => {
+export const getMovies = (args) => {
+  const[,sortMovieBy] = args.queryKey;
+  const[, pageNumPart] = args.queryKey; //based off of movies id below
+  const {pageNum} = pageNumPart; 
+  const {sort} =sortMovieBy;
+  console.log(pageNum)
   return fetch(
-      '/api/movies',{headers: {
-          'Authorization': window.localStorage.getItem('token')
-     }
-  }).then((response) => {
-      if (!response.ok) throw new Error(response.json().message);
-      return response.json();
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${pageNum}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
   })
-      .catch((e) => {
-          throw e
-      });
+  .catch((error) => {
+     throw error
+  });
 };
+
+// export const getMovies = () => {
+//   return fetch(
+//       '/api/movies',{headers: {
+//           'Authorization': window.localStorage.getItem('token')
+//      }
+//   }).then((response) => {
+//       if (!response.ok) throw new Error(response.json().message);
+//       return response.json();
+//   })
+//       .catch((e) => {
+//           throw e
+//       });
+// };
 
 export const getMovie = (args) => {
   const [, idPart] = args.queryKey;
